0ff1e3cd7f4d644106aff020cb05eec8ca9d7cad
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-03-10 23:32:11 +0900

    user index

---
+++ db/migrate/20210310123941_add_details_to_users.rb
@@ -0,0 +1,15 @@
+class AddDetailsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :gender, :string
+    add_column :users, :age, :string
+    add_column :users, :kind, :string
+    add_column :users, :address, :string
+    add_column :users, :phone, :string
+    add_column :users, :prefecture, :string
+    add_column :users, :nihongo, :string
+    add_column :users, :note, :string
+    add_column :users, :work, :boolean
+    add_column :users, :extra, :string
+  end
+end
