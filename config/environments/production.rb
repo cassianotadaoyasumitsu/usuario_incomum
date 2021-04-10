0bb9a1d1588bfa9bfaeae5fd5f4e1b5dcf0ccfb1
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-04-10 21:57:28 +0900

    staging url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://www.usuarioincomum.com" }
+  config.action_mailer.default_url_options = { host: "https://dashboard.heroku.com/apps/serene-badlands-37495" }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
