96d82c59fad0aa4cbe6c4d4f19af6d8450f900fc
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-05-02 22:02:29 +0900

    url changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://serene-badlands-37495.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://www.usuarioincomum.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
