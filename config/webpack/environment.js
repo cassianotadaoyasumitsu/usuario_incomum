dcc5d070f771fcaac11f4ff856673f9460adf438
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-08-29 09:20:43 +0900

    kaminari on keeps

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
-const webpack = require('webpack');
-// Preventing Babel from transpiling NodeModules packages
-environment.loaders.delete('nodeModules');
-// Bootstrap 4 has a dependency over jQuery & Popper.js:
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-);
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
 module.exports = environment
-
-
-
