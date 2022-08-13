9099c7be1d7cb932205571ee082ffb7c7d57edf9
cassianotadaoyasumitsu <cassiano@usuarioincomum.com>
2022-08-13 22:18:57 +0900

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     address: 'smtp.zoho.com',
     port: 465,
     user_name: 'contato@usuarioincomum.com',
-    password: EMAIL,
+    password: PASS,
     authentication: 'plain',
     ssl: true,
     tls: true,
